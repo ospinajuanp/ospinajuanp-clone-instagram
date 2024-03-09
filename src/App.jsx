a4903a5f2c1b126acef8f5dca8eab079e18ce0ad
Ospinajuanp <juanpo996@gmail.com>
2024-03-08 19:39:59 -0500

    Add component LowerMenuIcons Type Phone/Table

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import Profile from './page/Profile'
 import './App.css'
 
 function App() {
-
+  
   return (
     <main className='App'> 
       <h1>CLON INSTAGRAM</h1>
       <Profile/>
+      
     </main>
   )
 }
--- src/container/LowerMenuIcons.jsx
+++ src/container/LowerMenuIcons.jsx
@@ -1,9 +1,45 @@
+import { GoHome } from "react-icons/go";
+import { TiCompass } from "react-icons/ti";
+import { MdOutlineAddBox } from "react-icons/md";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+import { BiMoviePlay } from "react-icons/bi";
 import './LowerMenuIcons.css'
 
 function LowerMenuIcons() {
     return (
         <div className="LowerMenuIcons">
-            
+            <ul>
+                <li>
+                    <a href="/" rel="noopener noreferrer">
+                        <GoHome />
+                    </a>
+                </li>
+                <li>
+                    <a href="/" rel="noopener noreferrer">
+                        <TiCompass />
+                    </a>
+                </li>
+                <li>
+                    <a href="/" rel="noopener noreferrer">
+                        <BiMoviePlay />
+                    </a>
+                </li>
+                <li>
+                    <a href="/" rel="noopener noreferrer">
+                        <MdOutlineAddBox />
+                    </a>
+                </li>
+                <li>
+                    <a href="/" rel="noopener noreferrer">
+                        <IoPaperPlaneOutline />
+                    </a>
+                </li>
+                <li>
+                    <a href="/" rel="noopener noreferrer">
+                        <img className='photoProfile' src="https://unavatar.io/ospinajuanp" alt="ospinajuanp" />
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
